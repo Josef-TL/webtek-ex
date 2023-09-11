6fca4a0f3f72d893930dcfa8a28ea85ac3eacb21
jose-tl <j.tonnesenlucas@gmail.com>
2023-09-11 11:01:48 +0200

    js exercises

---
+++ main.js
@@ -0,0 +1,27 @@
+/*
+let greeting = "Hej!"
+console.log(greeting)
+console.log(greeting)
+console.log(greeting)
+*/
+/*
+console.log('I\'m awesome!');
+
+ */
+/*
+let numberOfStudents = 6
+let numberOfteachers = 36
+
+console.log(`Total number of teachers and students: ${numberOfStudents+numberOfteachers}`)
+
+console.log(`percentage of teachers: ${(numberOfteachers/(numberOfteachers+numberOfStudents))*100}`)
+console.log(`percentage of students: ${(numberOfStudents/(numberOfteachers+numberOfStudents))*100}`)
+ */
+
+
+let favoritePizza = "Pepperoni"
+let favoritePizzaPrice = 90
+let amountOfPizzas = prompt("Please enter amount of pizzas",1)
+let totalPrice = favoritePizzaPrice*amountOfPizzas
+
+console.log(`New pizza order: ${amountOfPizzas} ${favoritePizza}. Total cost for the order is: ${totalPrice}`)

e7c2723587bc3523d2ac2160f07c4272c0e4a4b9
jose-tl <j.tonnesenlucas@gmail.com>
2023-09-11 13:23:41 +0200

    js advanced exercise

--- main.js
+++ main.js
@@ -18,10 +18,18 @@ console.log(`percentage of teachers: ${(numberOfteachers/(numberOfteachers+numbe
 console.log(`percentage of students: ${(numberOfStudents/(numberOfteachers+numberOfStudents))*100}`)
  */
 
-
+/*
 let favoritePizza = "Pepperoni"
 let favoritePizzaPrice = 90
 let amountOfPizzas = prompt("Please enter amount of pizzas",1)
 let totalPrice = favoritePizzaPrice*amountOfPizzas
 
 console.log(`New pizza order: ${amountOfPizzas} ${favoritePizza}. Total cost for the order is: ${totalPrice}`)
+
+ */
+
+let yearOfBirth = prompt("What year were you born?",1997)
+let yearFuture = prompt("What year is the future?",2050)
+let age = yearFuture-yearOfBirth
+
+console.log(`You will be ${age} years old in ${yearFuture}`)
